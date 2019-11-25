59ac063d919735c470580a6a24c8b06445357870
Augyu <yuhsuan@vt.edu>
2019-11-25 02:36:37 -0500

    Geo Coding

---
+++ src/component/DBQuery.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import axios from "axios";
+require('dotenv').config()
+
+class DBQuery extends Component {
+  state = {
+    crimeData: {},
+    latAndLong: {}
+  };
+  getUsers = () => {
+    axios
+      .get(process.env.REACT_APP_SERVER_URL+'data')
+      .then(data => this.setState({ crimeData: data.data }))
+      .catch(err => {
+        console.log(err);
+        return null;
+      });
+  };
+  render() {
+    return <div>{JSON.stringify(this.state.latAndLong)}</div>;
+  }
+  componentDidMount() {
+    this.getUsers();
+    
+  }
+  reverseGeoCoding = () => {
+      axios
+        .get('https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key='+process.env.REACT_APP_GOOGLE_API)
+        .then(data => this.setState({ latAndLong: data}))
+        .catch(err => {
+            console.log(err);
+            return null;
+        })
+  }
+}
+
+export default DBQuery;

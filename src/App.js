f37b560148c1d74afd2377843f5507ef1371301b
Augyu <yuhsuan@vt.edu>
2019-12-02 16:19:47 -0500

    Fix buges

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
-import GoogleMap from './component/GoogleMap';
+import React, { Component } from "react";
+import "./App.css";
+import GoogleMap from "./component/GoogleMap";
 //import DBQuery from './component/DBQuery';
-import CrimeSearch from './component/search'
-import Filter from './component/Filter'
+import Filter from "./component/Filter";
 import axios from "axios";
 
-require('dotenv').config()
+require("dotenv").config();
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
-      update: false,
-    }
+      update: false
+    };
     this.handler = this.handler.bind(this);
   }
   componentDidMount() {
     axios
-      .get(process.env.REACT_APP_SERVER_URL+'data')
+      .get(process.env.REACT_APP_SERVER_URL + "data")
       .then(res => {
         res.data.forEach(result => {
           result.show = false;
         });
-        this.setState({data: res.data});
+        this.setState({ data: res.data });
       })
       .catch(err => {
         console.log(err);
@@ -32,19 +31,19 @@ class App extends Component {
       });
   }
   handler() {
-    this.setState({ update: true })
+    this.setState({ update: true });
   }
-  render() {  
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>Washington D.C. Crime Search</p>
         </header>
-        <body className='search'>
-          <CrimeSearch />
-        </body>
-        <GoogleMap data={this.state.data} update={this.state.update}/>
-        <Filter data={this.state.data} action={this.handler}/>
+        <div className="search">
+          {/*<CrimeSearch />*/}
+          <Filter data={this.state.data} action={this.handler} />
+        </div>
+        <GoogleMap data={this.state.data} />
       </div>
     );
   }
--- src/component/Filter.js
+++ src/component/Filter.js
@@ -1,53 +1,57 @@
-import React, { Component } from 'react';
-import './Filter.css';
+import React, { Component } from "react";
+import "./Filter.css";
 
-
-class Filter extends Component{
-  constructor(props){
+class Filter extends Component {
+  constructor(props) {
     super(props);
     this.state = {
-			type: '',
-			options: ['THEFT/OTHER','HOMICIDE','THEFT F/AUTO'],
-			selectValue: '',
-			renderButton:''
-    }
-	}
+      value: "THEFT/OTHER",
+      options: ["THEFT/OTHER", "HOMICIDE", "THEFT F/AUTO"],
+      selectValue: "",
+      renderButton: ""
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleChange = this.handleChange.bind(this);  
+  }
 
-	setShow = (data) => {
-		data.map(crime => (
-			(crime.offense === 'THEFT/OTHER') ? crime.show=true :null	
-		));
-		console.log(data);
-		//update = {this.props.action}
+  update = () => {
+    this.props.action();
+  };
+  setShow = () => {
+    this.props.data.map(crime =>
+      crime.offense === this.state.value ? (crime.show = true) : (crime.show = false)
+    );
+    this.update();
+	};
+	handleSubmit(event) {
+		this.setShow()
+		event.preventDefault();
+	}
+	handleChange(event) {
+		this.setState({value: event.target.value});
 	}
   render() {
-		this.setShow(this.props.data)
     return (
-			<div>
-				<p>haha</p>
-				<button onClick={this.props.action} />
-			</div>
+      <div className="Filter">
+				<form onSubmit={this.handleSubmit}>
+        <label>
+          Crime Type:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="THEFT/OTHER">THEFT/OTHER</option>
+            <option value="HOMICIDE">HOMICIDE</option>
+            <option value="ROBBERY">ROBBERY</option>
+            <option value="MOTOR VEHICLE THEFT">MOTOR VEHICLE THEFT</option>
+						<option value="SEX ABUSE">SEX ABUSE</option>
+						<option value="THEFT F/AUTO">THEFT F/AUTO</option>
+						<option value="ASSAULT W/DANGEROUS WEAPON">ASSAULT W/DANGEROUS WEAPON</option>
+						<option value="BURGLARY">BURGLARY</option>
+						<option value="ARSON">ARSON</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      	</form>
+      </div>
     );
-	}
-}
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
   }
 }
 
